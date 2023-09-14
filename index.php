6f26c3c3a4ae8c443301104a2817f2ba449fc8fe
Samuel Girard <sgirard2004@icloud.com>
2023-09-14 17:15:35 -0400

    Création des pages du menu

--- index.php
+++ index.php
@@ -8,13 +8,25 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Menu</title>
         <link rel="stylesheet" href="css/index.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
 
     <body>
-        <h1>Menu</h1>
+        <div class="container-fluid">
+            <div class="row text-center">
+                <div class="col-md-2 offset-3">
+                    <a href="evenement.php"><h1>Événement</h1></a>
+                </div>
+                <div class="col-md-2">
+                    <a href="creationEvent.php"><h1>Créer un événement</h1></a>
+                </div>
+                <div class="col-md-2">
+                    <a href="sourire.php"><h1>Sourire</h1></a>
+                </div>
+            </div>
+        </div>
 
 
 
